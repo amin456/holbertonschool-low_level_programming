8d6ddc900f8fd381f572c56e9c854e40098b20aa
amin456 <1408@holbertonschool.com>
2020-02-05 14:22:44 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
  *main - function
+ *main function includes printf function
  *Return: 0
  */
-
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,");
-
+	printf("with proper grammar\, but the outcome is a piece of art\,");
 	return (0);
-
 }
