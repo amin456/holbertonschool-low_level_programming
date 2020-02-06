fc018f8436bef3e3a35e51ce6fb1da909af99a8f
amin456 <1408@holbertonschool.com>
2020-02-06 22:48:46 +0000

    commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,33 +1,19 @@
 #include <stdio.h>
-
 /**
-
  *main - main function
-
  *@void: empty
-
  *Return: 0
-
  */
-
 int main(void)
 
 {
+char c = 'z';
+char c2 = '\n';
 
-	char c = 'z';
-
-	char c2 = '\n';
-
-	for (c = 'z'; c >= 'a'; c--)
-
-	{
-
-		putchar (c);
-
-	}
-
-	putchar (c2);
-
-	return (0);
-
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar (c);
+}
+putchar (c2);
+return (0);
 }
