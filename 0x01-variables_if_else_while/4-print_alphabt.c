9e0f6f04b6834aedeebf2334ab587879c90947e2
amin456 <1408@holbertonschool.com>
2020-02-06 23:36:11 +0000

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,41 +1,23 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-
 /**
-
  * main - alphabet
-
  * Return: (0)
-
  */
-
 int main(void)
-
 {
-
-	char c;
-
-	c = 'a';
+char c;
+char = 'a';
+char c2 = '\n';
 
 	for (c = 'a'; c <= 'z'; c++)
-
 	{
-
 		if (c != 'q' && c != 'e')
-
 		{
-
 			putchar(c);
-
 		}
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
