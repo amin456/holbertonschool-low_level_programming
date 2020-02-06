c9b66aad7d1e6103d7e6c31dcdab1f887d3c98c9
amin456 <1408@holbertonschool.com>
2020-02-06 21:03:08 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	char c2 = '\n';
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar (c);
+	}
+	putchar (c2);
+	return (0);
+}
