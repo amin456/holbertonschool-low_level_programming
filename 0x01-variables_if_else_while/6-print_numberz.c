519a3fafc5e860107a0be270f61ebc7405a7410a
amin456 <1408@holbertonschool.com>
2020-02-06 23:08:45 +0000

    commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+B#include <stdio.h>
 /**
  * main - alphabet
  * Return: (0)
@@ -7,6 +7,7 @@ int main(void)
 {
 	int c;
 	c = 48;
+	
 	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
