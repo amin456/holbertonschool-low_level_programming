fc42e324325c6e16a2d615348ef3dc558737ff5d
amin456 <1408@holbertonschool.com>
2020-02-06 20:49:50 +0000

    positive or nagative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/*more headers goes there */
+/* more headers goes there */
 /**
  *main - main function
  *function to find out if a variable is positive or negative
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
