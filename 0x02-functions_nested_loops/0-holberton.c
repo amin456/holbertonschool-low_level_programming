d46db4dffee4069a573a44cdd8b4dcfe2ecfa8ad
amin456 <1408@holbertonschool.com>
2020-02-11 08:29:29 +0000

    0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - main function
+ * Description: a program that prints Holberton, followed by a new line
+ *Return: 0
+ */
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
