77c77719febb274d4541cab500aff4917f582197
amin456 <1408@holbertonschool.com>
2020-02-11 10:41:05 +0000

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10 - main function
+ *return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i, h;
+
+	h = 0;
+
+	while (h < 10)
+	{
+		i = 'a';
+		while (i <= 'z')
+		{
+			_putchar(i++);
+		}
+		_putchar(10);
+	}
+}
