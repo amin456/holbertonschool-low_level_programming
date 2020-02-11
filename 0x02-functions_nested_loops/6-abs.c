a2457402376c16bf191cf788e15dc1f237c2e028
amin456 <1408@holbertonschool.com>
2020-02-11 12:26:24 +0000

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *main - main function
+ *Description:computes the absolute value of an integer
+ *n: int
+ *Return: 0
+ */
+int _abs(int)
+{
+	if (n < 0)
+		n = n * -1;
+	return (n);
+}
