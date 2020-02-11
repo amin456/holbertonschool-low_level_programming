e494e2737c0f34ea49928e32beab08c0b658e904
amin456 <1408@holbertonschool.com>
2020-02-11 21:25:21 +0000

    65

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,35 @@
 #include "holberton.h"
 /**
- *main - main function
- *Description: a program that prints Holberton, followed by a new line
- *Return: 0
+ * times_table - main function
+ * @void: empty
+ * Return: _putchar
  */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(' ');
+			if (i * j < 10)
+			{
+				_putchar(' ');
+				_putchar(i * j + '0');
+			}
+			else
+			{
+				_putchar(i * j / 10 + '0');
+				_putchar(i * j % 10 + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar ('\n');
+	}
+}
