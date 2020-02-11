ee7e64f5b6683f6216f354ca79dd67fe51d19588
amin456 <1408@holbertonschool.com>
2020-02-11 13:44:01 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *add - main function
+ *@x: para
+ *@y: para
+ *Description:
+ *Return: z
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = (x + y);
+	return (z);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,6 @@
+#include "holberton.h"
+/**
+ *main - main function
+ *Description: a program that prints Holberton, followed by a new line
+ *Return: 0
+ */
