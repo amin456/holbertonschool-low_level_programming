71e32aff75ea1bfd3960750d99900e65f756746b
amin456 <1408@holbertonschool.com>
2020-02-11 13:16:21 +0000

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *jack_bauer - main function
+ *@void: para
+ *Description: a program that prints Holberton, followed by a new line
+ *Return:
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
