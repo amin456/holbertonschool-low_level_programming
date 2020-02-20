61bc0df3e1767ec76cebb1b5ff3ead15464044b9
amin456 <1408@holbertonschool.com>
2020-02-20 18:00:00 +0000

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,16 +7,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a = 0, b = 0;
+	int a = 0, b;
 
 	while (dest[a] != '\0')
 		a++;
-	while (src[b] != '\0')
+	for (b = 0; src[b] != '\0'; b++)
 	{
-		dest[a] = src[b];
-		a++;
-		b++;
+		dest[a+b] = src[b];
 	}
-	dest[a + b] = '\0';
 	return (dest);
 }
