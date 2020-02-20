f7016f2c197d88477163e0c1e9cf37ea6b18d75d
amin456 <1408@holbertonschool.com>
2020-02-20 09:55:33 +0000

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * char *_strcat - main function
+ * @dest: destination
+ * @src: source
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a + b + 1] = '\0';
+	return (dest);
+}
