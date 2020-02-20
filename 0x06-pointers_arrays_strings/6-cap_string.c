775e2d10f6f1a6acc0001fd9601588d1c8c61493
amin456 <1408@holbertonschool.com>
2020-02-20 22:26:16 +0000

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,23 @@
 #include "holberton.h"
 /**
- * *cap_string - main function
- * @s: str
- * Return: s
+ *cap_string- uppercase for first letter
+ *@m: pointer
+ *Return: return m
  */
-char *cap_string(char *s)
+char *cap_string(char *m)
 {
-	int a, b;
-	char m[] = {' ', '\t', ',', ';', '?', '.', '!', '"', '(', ')', '{', '}'};
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s[0] -= 32;
-	for (a = 0; s[a] != '\0'; a++)
+	int a;
+
+	for (a = 0; m[a] != '\0'; a++)
 	{
-		if (s[a] >= 'a' && s[a] <= 'z')
-			for (b = 0; m[b] != '\0'; b++)
-				if ((s[a - 1]) == m[b])
-				{
-					s[a] -= 32;
-				}
+		if ((m[a] == '\n') || (m[a] == '\t') || (m[a] == ' ') || (m[a] == '}')
+		    || (m[a] == '{') || (m[a] == ')') || (m[a] == '(') || (m[a] == '"')
+		    || (m[a] == '?') || (m[a] == '!') || (m[a] == '.') || (m[a] == ';')
+		    || (m[a] == ','))
+		{
+			if ((m[a + 1] >= 'a') && (m[a + 1] <= 'z'))
+				m[a + 1] = m[a + 1] - 32;
+		}
 	}
-	return (s);
+	return (m);
 }
