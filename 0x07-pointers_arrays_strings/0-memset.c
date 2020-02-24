b3b5c992df0b88904690b64e3f2dea2a87242b92
amin456 <1408@holbertonschool.com>
2020-02-24 20:39:59 +0000

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_memset - main function
+ * @s: size
+ * @b: buff
+ * @n: para
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a = 0;
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
