b1b99382075689dd3803f4149a68cf5b8dbc5061
amin456 <1408@holbertonschool.com>
2020-03-04 21:05:19 +0000

    .

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,21 +3,21 @@
 /**
  * *_strdup - main function
  * @str: para
- * Return: m
+ * Return: s
  */
 char *_strdup(char *str)
 {
-	char *m;
+	char *s;
 	unsigned int x, y;
 
 	if (str == NULL)
 		return (NULL);
 	for (x = 0; str[x] != '\0'; x++)
-		m = malloc(x + 1);
-	if (m == NULL)
+		s = malloc(x + 1);
+	if (s == NULL)
 		return (NULL);
 	for (y = 0; y <= x; y++)
-		m[y] = str[y];
-	m[y] = '\0';
-	return (m);
+		s[y] = str[y];
+	s[y] = '\0';
+	return (s);
 }
